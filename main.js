e936b8cb145d2b873c8a8c8e97c7bd496ac0874f
solveighelga <111748545+solveighelga@users.noreply.github.com>
2022-09-14 13:15:24 +0000

    added more comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// add contact
+/* add contact */
 let contacts = [];
 
 const add = (contact)=>{ //This us a function expression (contact=parameter, everything iside the {} is the body of my function)
@@ -15,22 +15,23 @@ const add = (contact)=>{ //This us a function expression (contact=parameter, eve
         return;
     }
 
-    contacts.push(contact); // add something to the contact I use the push method because it is a Array
+    contacts.push(contact); // add something to the contact I use the push method
     console.log("was added");
 
 }
 
+// Here are emails and names I will be using to demonstrate the following: add, remove email, edit email, get email, listAll and Clear.
 add({email:"solveighelga@gmail.com",name:"Sólveig"});
 add({email:"solveighelgaa@gmail.com",name:"Sólveigh"});
 add({email: "shh23@hi.is", name:"Sólveig"});
 console.log("after add");
 
-// remove email
+/* remove email */
 const remove = (email)=>{
 
     for(let i=0; i<contacts.length; i++)
         if(email==contacts[i].email){ // here I want to see if the email I input already exist in contacts
-            console.log("Removing email"); //If so we remove item with splice
+            console.log("Removing email"); //If so it will remove item with splice
             contacts.splice(i, 1); // 2nd parameter means remove one item only
             return;
         }
@@ -43,7 +44,7 @@ const remove = (email)=>{
 remove("solveighelga@gmail.com");
 console.log("was removed");
 
-// edit email, newData
+/* edit email, newData */
 const edit = (email, newData)=>{ // newData is my object
 
     for(let i=0; i<contacts.length; i++)
@@ -58,12 +59,12 @@ const edit = (email, newData)=>{ // newData is my object
     return;
  }
 
-edit("shh23@hi.is", {email:"smh23@hi.is", name:"Sigrún"}); // here I edited both the email and name for shh23@hi.is email
+edit("shh23@hi.is", {email:"smh23@hi.is", name:"Sigrún"}); // here I edited both the email and name for shh23@hi.is
 console.log(contacts);
-edit("smh23@hi.is", {email:"sigga@gmail.com"});
+edit("smh23@hi.is", {email:"sigga@gmail.com"}); // tested it again
 console.log(contacts);
 
-// Get email
+/* Get email */
 const get = (email)=>{
 
     for(let i=0; i<contacts.length; i++)
@@ -80,25 +81,25 @@ const get = (email)=>{
     return;
  }
 
- get("solveighelgaa@gmail.com"); // I wanted to find the name and email of this contact (unfer //add contact section)
+ get("solveighelgaa@gmail.com"); // I wanted to find the name and email of this contact (under /* add contact */ section)
 
-// listAll()
+/* listAll() */
 const listAll = ()=>{
     for(let i=0; i<contacts.length; i++)
-        console.log(contacts[i].name + " " + contacts[i].email)
+        console.log(contacts[i].name + " " + contacts[i].email) // I wanted to list all contacts[i] it this perticular oder, name first then email.
 }
 
-add({email:"anna@tskoli.is",name:"Anna Valsdóttir"});
+add({email:"anna@tskoli.is",name:"Anna Valsdóttir"}); // added some extra contact from the guide
 add({email:"jon@tskoli.is",name:"Jón Jónsson"});
 add({email: "hildur@tskoli.is", name:"Hildur Eriksdóttir"});
 
 listAll();
 
-// Clear()
+/* Clear() */
 const Clear = ()=>{
     for(let i=0; i<contacts.length; i++)
-            contacts.splice(contacts[i]);
+            contacts.splice(contacts[i]); // I used the same method as in /* remove email */ except now i wanted to remove everyone from the list
 }
 
 Clear();
-console.log(contacts);
+console.log(contacts); // double checked to make sure everyone was removed :)
